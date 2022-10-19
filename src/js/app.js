078366501ff42ac0d4527c121ff56118a8752604
qwertyuiop13579 <harlap1999@mail.ru>
2022-10-19 20:30:14 +0300

    add header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
-import * as functions from './modules/functions.js';
+// Burger icom menu
+const iconMenu = document.querySelector('.menu__icon');
+const menu = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+        iconMenu.classList.toggle('opened');
+        menu.classList.toggle('opened');
+    });
 
-console.log('Hello world!');
+}
