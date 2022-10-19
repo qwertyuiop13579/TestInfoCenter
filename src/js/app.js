0cc3e34c632beddf61dfa18a3ed21b57af7b1454
qwertyuiop13579 <harlap1999@mail.ru>
2022-10-20 02:28:11 +0300

    start order block

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-// Burger icom menu
+// Burger icon menu
 const iconMenu = document.querySelector('.menu__icon');
 const menu = document.querySelector('.menu__body');
 if (iconMenu) {
@@ -6,5 +6,22 @@ if (iconMenu) {
         iconMenu.classList.toggle('opened');
         menu.classList.toggle('opened');
     });
-
 }
+
+// Slider value
+var slider = document.getElementById("range__input");
+var output = document.getElementById("range__value");
+output.innerHTML = slider.value + "%";
+
+slider.oninput = function () {
+    output.innerHTML = this.value + "%";
+}
+
+
+// Submitting form
+var form = document.querySelector(".order__form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('Submit form');
+    alert('Submit form');
+})
